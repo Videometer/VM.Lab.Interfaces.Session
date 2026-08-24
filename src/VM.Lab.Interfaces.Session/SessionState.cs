b067de7bd2dc92e1cb1205163af8ac97c6e207ff
KRT <krt@videometer.com>
2026-08-24 13:54:05 +0200

    Added new session states

---
+++ src/VM.Lab.Interfaces.Session/SessionState.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable InconsistentNaming
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+
+namespace VM.Lab.Interfaces.Session;
+
+/// <summary>
+/// Enumerator with session states.
+/// See the session state machine visualization graph.
+/// </summary>
+public enum SessionState
+{
+    Idle,
+    Connecting,
+    Ready,
+    AdjustingLightSetup,
+    AdjustingLightSetupFailed,
+    Capturing,
+    Processing,
+    Waiting,
+    Error
+}
+
