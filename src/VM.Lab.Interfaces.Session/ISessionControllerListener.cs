6e0b3674760e3375052e1a0616247f4b62991366
KRT <krt@videometer.com>
2026-08-24 14:07:24 +0200

    Added ISessionControllerListener and SessionController

---
+++ src/VM.Lab.Interfaces.Session/ISessionControllerListener.cs
@@ -0,0 +1,20 @@
+﻿namespace VM.Lab.Interfaces.Session;
+
+/// <summary>Interface for controlling a session</summary>
+public interface ISessionControllerListener
+{
+    /// <summary>Start a new measurement</summary>
+    /// <param name="id">ID of the sample</param>
+    /// <param name="initials">Operator initials</param>
+    /// <param name="comments">Operator comments</param>
+    void Capture(string id, string initials, string comments);
+
+    /// <summary>Pause the current capture process</summary>
+    void Pause();
+
+    /// <summary>Finish the measurement</summary>
+    void Finish();
+
+    /// <summary>Starts a new measurement</summary>
+    void New();
+}
---
+++ src/VM.Lab.Interfaces.Session/SessionController.cs
@@ -0,0 +1,27 @@
+﻿namespace VM.Lab.Interfaces.Session;
+
+/// <summary>Implement this to be an agent that controls sessions</summary>
+public abstract class SessionController
+{
+    /// <summary>Implementer of session control commands</summary>
+    protected readonly ISessionControllerListener _listener;
+
+    /// <summary>An agent that controls sessions</summary>
+    /// <param name="listener"></param>
+    protected SessionController(ISessionControllerListener listener)
+    {
+        _listener = listener;
+    }
+
+    /// <summary>Occurs when the state has changed</summary>
+    /// <param name="previousState"></param>
+    /// <param name="newState"></param>
+    public abstract void StateChanged(SessionState previousState, SessionState newState);
+
+    /// <summary>
+    /// Called when something in the pipeline of capturing, analysing, or saving of results, failed for the current image.
+    /// Used to inform external controllers that something went wrong with the current image. 
+    /// </summary>
+    /// <param name="errorMessage">Error message explaining why the analysis failed</param>
+    public abstract void LastImageFailed(string errorMessage);
+}
