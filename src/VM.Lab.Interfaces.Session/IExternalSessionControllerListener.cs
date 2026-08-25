fbaca0c01ee7e830725fd3efc6cef0f09fc68368
KRT <krt@videometer.com>
2026-08-25 14:12:46 +0200

    Moved methods from ILightSetupAdjuster to IExternalSessionControllerListener

--- src/VM.Lab.Interfaces.Session/IExternalSessionControllerListener.cs
+++ src/VM.Lab.Interfaces.Session/IExternalSessionControllerListener.cs
@@ -1,10 +1,17 @@
 ﻿namespace VM.Lab.Interfaces.Session;
 
-/// <summary>
-/// Interface implemented by an external controller of session used to adjust light setup fo the currently used imaging device
-/// </summary>
-public interface ILightSetupAdjuster
+/// <summary>Interface for controlling a session</summary>
+public interface ISessionControllerListener
 {
+    /// <summary>Start a new measurement</summary>
+    /// <param name="id">ID of the sample</param>
+    /// <param name="initials">Operator initials</param>
+    /// <param name="comments">Operator comments</param>
+    void Capture(string id, string initials, string comments);
+    
+    /// <summary>Starts a new measurement</summary>
+    void New();
+    
     /// <summary>Loads a specified light setup configuration into the device</summary>
     /// <param name="lightSetupName">The name of the light setup file to load</param>
     void LoadLightSetup(string lightSetupName);
