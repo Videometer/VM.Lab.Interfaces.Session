29b9726def0d1683f0b7ab8c48213631afb60864
KRT <krt@videometer.com>
2026-08-26 14:04:45 +0200

    Added test GUI for ExternalSerialSessionController in solution

---
+++ src/VM.Lab.Session.ExternalSerialSessionController.TestGui/DummyExternalSessionControllerListener.cs
@@ -0,0 +1,38 @@
+﻿using VM.Lab.Interfaces.Session;
+
+namespace VM.Lab.Session.ExternalSerialSessionController.TestGui;
+
+public class DummyExternalSessionControllerListener : IExternalSessionControllerListener
+{
+    public event EventHandler CaptureCalled;
+    
+    public void Capture(string id, string initials, string comments)
+    {
+        CaptureCalled?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void New()
+    {
+        Console.WriteLine($"{nameof(New)} was called.");
+    }
+
+    public void Stop()
+    {
+        Console.WriteLine($"{nameof(Stop)} was called.");
+    }
+
+    public void LoadLightSetup(string lightSetupName)
+    {
+        Console.WriteLine($"{nameof(LoadLightSetup)} was called.");
+    }
+
+    public void SaveLightSetup(string lightSetupName)
+    {
+        Console.WriteLine($"{nameof(SaveLightSetup)} was called.");
+    }
+
+    public void DoAutoLight()
+    {
+        Console.WriteLine($"{nameof(DoAutoLight)} was called.");
+    }
+}
---
+++ src/VM.Lab.Session.ExternalSerialSessionController.TestGui/Program.cs
@@ -0,0 +1,18 @@
+﻿using VM.Lab.Interfaces.Session;
+using VM.Lab.Session.ExternalSerialSessionController.TestGui;
+using VM.Lab.Session.SerialSessionController;
+
+Console.WriteLine("This program is used to receive commands from the Python scripts and check that the 'External Serial Session Controller' receives and understands the commands.");
+
+var listener = new DummyExternalSessionControllerListener();
+var controller = new ExternalSerialSessionController(listener);
+listener.CaptureCalled += (_, _) => 
+{
+    controller.StateChanged(SessionState.Capturing);
+    controller.StateChanged(SessionState.Processing);
+    controller.StateChanged(SessionState.Ready);
+};
+
+// Simulate that the session runner window has been opened and we have connected to the device
+controller.StateChanged(SessionState.Ready); 
+
